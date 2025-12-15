9b5510f8344e41721e278e38b9637ae18ea61e1e
Raphaël Dubigny <raphael.dubigny@beta.gouv.fr>
2025-12-15 17:07:41 +0100

    Build version 0.14.3-proconnect.1

---
+++ types/decorator/common/ValidatePromise.d.ts
@@ -0,0 +1,6 @@
+import { ValidationOptions } from '../ValidationOptions';
+export declare const VALIDATE_PROMISE = "validatePromise";
+/**
+ * Resolve promise before validation
+ */
+export declare function ValidatePromise(validationOptions?: ValidationOptions): PropertyDecorator;
---
+++ types/metadata/ValidationMetadataArgs.d.ts
@@ -0,0 +1,38 @@
+import { ValidationOptions } from '../decorator/ValidationOptions';
+/**
+ * Constructor arguments for ValidationMetadata class.
+ */
+export interface ValidationMetadataArgs {
+    /**
+     * Validation type. Should be one of the ValidationTypes values.
+     */
+    type: string;
+    /**
+     * Validation name. Used to uniquely identify this validator.
+     */
+    name: string;
+    /**
+     * Object that is used to be validated.
+     */
+    target: Function | string;
+    /**
+     * Property of the object to be validated.
+     */
+    propertyName: string;
+    /**
+     * Constraint class that performs validation. Used only for custom validations.
+     */
+    constraintCls?: Function;
+    /**
+     * Array of constraints of this validation.
+     */
+    constraints?: any[];
+    /**
+     * Validation options.
+     */
+    validationOptions?: ValidationOptions;
+    /**
+     * Extra options specific to validation type.
+     */
+    validationTypeOptions?: any;
+}
